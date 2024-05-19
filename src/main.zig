16cd9fe7947e5c4cea70d49c8dad4af98ae9df29
Scott Lambert <s-lambert@live.com.au>
2024-05-19 09:23:18 +0800

    Basic sprites

--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,122 @@
 const std = @import("std");
-const sokol = @import("sokol");
+const jok = @import("jok");
+const sdl = jok.sdl;
+const j2d = jok.j2d;
 
-export fn init() void {}
+pub const jok_window_borderless = true;
 
-export fn frame() void {}
+var sheet: *j2d.SpriteSheet = undefined;
+var animator: *j2d.AnimationSystem = undefined;
+var animation: []const u8 = "player_down";
+var flip_h = false;
 
-export fn input(_: ?*const sokol.app.Event) void {}
+const velocity = 100;
+var pos = sdl.PointF{ .x = 200, .y = 200 };
 
-export fn cleanup() void {}
+pub fn init(ctx: jok.Context) !void {
+    const size = ctx.getCanvasSize();
+    sheet = try j2d.SpriteSheet.fromPicturesInDir(
+        ctx,
+        "assets/images",
+        @intFromFloat(size.x),
+        @intFromFloat(size.y),
+        1,
+        true,
+        .{},
+    );
+    animator = try j2d.AnimationSystem.create(ctx.allocator());
+    const player = sheet.getSpriteByName("player").?;
+    try animator.add(
+        "player_left_right",
+        &[_]j2d.Sprite{
+            player.getSubSprite(16 * 2, 24 * 1, 16, 24),
+            player.getSubSprite(16 * 1, 24 * 1, 16, 24),
+            player.getSubSprite(16 * 3, 24 * 1, 16, 24),
+            player.getSubSprite(16 * 1, 24 * 1, 16, 24),
+        },
+        6,
+        false,
+    );
+    try animator.add(
+        "player_down",
+        &[_]j2d.Sprite{
+            player.getSubSprite(16 * 2, 24 * 0, 16, 24),
+            player.getSubSprite(16 * 1, 24 * 0, 16, 24),
+            player.getSubSprite(16 * 3, 24 * 0, 16, 24),
+            player.getSubSprite(16 * 1, 24 * 0, 16, 24),
+        },
+        6,
+        false,
+    );
+    try animator.add(
+        "player_up",
+        &[_]j2d.Sprite{
+            player.getSubSprite(16 * 2, 24 * 2, 16, 24),
+            player.getSubSprite(16 * 1, 24 * 2, 16, 24),
+            player.getSubSprite(16 * 3, 24 * 2, 16, 24),
+            player.getSubSprite(16 * 1, 24 * 2, 16, 24),
+        },
+        6,
+        false,
+    );
+}
 
-pub fn main() !void {
-    sokol.app.run(.{
-        .init_cb = init,
-        .frame_cb = frame,
-        .event_cb = input,
-        .cleanup_cb = cleanup,
-        .width = 500,
-        .height = 500,
-        .window_title = "game.zig",
-        .logger = .{ .func = sokol.log.func },
-    });
+pub fn event(_: jok.Context, _: sdl.Event) !void {
+    // your event processing code
+}
+
+pub fn update(ctx: jok.Context) !void {
+    var force_replay = false;
+    if (ctx.isKeyPressed(.up)) {
+        pos.y -= velocity * ctx.deltaSeconds();
+        animation = "player_up";
+        flip_h = false;
+        force_replay = true;
+    } else if (ctx.isKeyPressed(.down)) {
+        pos.y += velocity * ctx.deltaSeconds();
+        animation = "player_down";
+        flip_h = false;
+        force_replay = true;
+    } else if (ctx.isKeyPressed(.right)) {
+        pos.x += velocity * ctx.deltaSeconds();
+        animation = "player_left_right";
+        flip_h = true;
+        force_replay = true;
+    } else if (ctx.isKeyPressed(.left)) {
+        pos.x -= velocity * ctx.deltaSeconds();
+        animation = "player_left_right";
+        flip_h = false;
+        force_replay = true;
+    }
+    if (force_replay and try animator.isOver(animation)) {
+        try animator.reset(animation);
+    }
+    animator.update(ctx.deltaSeconds());
+}
+
+pub fn draw(ctx: jok.Context) !void {
+    ctx.clear(sdl.Color.rgb(77, 77, 77));
+
+    j2d.begin(.{});
+    defer j2d.end();
+    try j2d.sprite(
+        try animator.getCurrentFrame(animation),
+        .{
+            .pos = pos,
+            .flip_h = flip_h,
+            .scale = .{ .x = 2, .y = 2 },
+        },
+    );
+    jok.font.debugDraw(
+        ctx,
+        .{ .x = 300, .y = 0 },
+        "Press up/down/left/right to move character around",
+        .{},
+    );
+}
+
+pub fn quit(_: jok.Context) void {
+    std.log.info("game quit", .{});
+    sheet.destroy();
+    animator.destroy();
 }
