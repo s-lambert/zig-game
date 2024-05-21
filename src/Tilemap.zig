694afaaaecb2ec7ca148f212b608947b749fea3c
Scott Lambert <s-lambert@live.com.au>
2024-05-21 23:25:53 +0800

    Start bounds checking for the tilemap

--- src/Tilemap.zig
+++ src/Tilemap.zig
@@ -34,24 +34,33 @@ const map = [constants.tiles_height * constants.tiles_width]u8{
     0, 1, 5, 25, 26, 27, 5, 0, 1, 5,
 };
 
+const bounds: sdl.RectangleF = .{ .x = 64.0, .y = 64.0, .width = 128.0, .height = 128.0 };
+
 pub fn draw(_: jok.Context) !void {
     const dungeon_tilemap = sheet.getSpriteByName("dungeon_tilemap").?;
-    for (map, 0..) |tile, tileNum| {
-        const colNum = tileNum % constants.tiles_width;
-        const rowNum = tileNum / constants.tiles_width;
+    for (map, 0..) |tile, tile_index| {
+        const tile_x = tile_index % constants.tiles_width;
+        const tile_y = tile_index / constants.tiles_width;
 
-        const tileColNum = tile % tilemap_columns;
-        const tileRowNum = tile / tilemap_columns;
+        const spritesheet_x = tile % tilemap_columns;
+        const spritesheet_y = tile / tilemap_columns;
+
+        const tile_pos = constants.tile_pos(tile_x, tile_y);
+        const tile_rect: sdl.RectangleF = .{ .x = tile_pos.x, .y = tile_pos.y, .width = 64.0, .height = 64.0 };
+
+        if (!constants.aabb_intersect(&bounds, &tile_rect)) {
+            continue;
+        }
 
         try j2d.sprite(
             dungeon_tilemap.getSubSprite(
-                @as(f32, @floatFromInt(tileColNum)) * 16.0,
-                @as(f32, @floatFromInt(tileRowNum)) * 16.0,
-                16,
-                16,
+                @as(f32, @floatFromInt(spritesheet_x)) * constants.tile_size_f,
+                @as(f32, @floatFromInt(spritesheet_y)) * constants.tile_size_f,
+                constants.tile_size_f,
+                constants.tile_size_f,
             ),
             .{
-                .pos = constants.tile_pos(colNum, rowNum),
+                .pos = tile_pos,
                 .scale = constants.tiles_scale,
             },
         );
--- src/constants.zig
+++ src/constants.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const jok = @import("jok");
 const sdl = jok.sdl;
 
@@ -15,4 +16,16 @@ pub fn tile_pos(x: usize, y: usize) sdl.PointF {
         .x = 0.0 + @as(f32, @floatFromInt(x)) * tile_size_f * tiles_mulitipler,
         .y = 0.0 + @as(f32, @floatFromInt(y)) * tile_size_f * tiles_mulitipler,
     };
+}
+
+pub fn aabb_intersect(rect_a: *const sdl.RectangleF, rect_b: *const sdl.RectangleF) bool {
+    const a_x_min = rect_a.x;
+    const a_x_max = rect_a.x + rect_a.width;
+    const a_y_min = rect_a.y;
+    const a_y_max = rect_a.y + rect_a.height;
+    const b_x_min = rect_b.x;
+    const b_x_max = rect_b.x + rect_b.width;
+    const b_y_min = rect_b.y;
+    const b_y_max = rect_b.y + rect_b.height;
+    return (a_x_min < b_x_max and a_x_max > b_x_min) and (a_y_min < b_y_max and a_y_max > b_y_min);
 }
