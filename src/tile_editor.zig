8eedb5202e3757c24a6d3fc4993f599f4358c89e
Scott Lambert <s-lambert@live.com.au>
2024-05-22 19:54:18 +0800

    Start rendering tilemap editor

--- src/tile_editor.zig
+++ src/tile_editor.zig
@@ -7,12 +7,28 @@ pub fn main() !void {
     rl.initWindow(screen_width, screen_height, "Editor");
     defer rl.closeWindow();
 
+    const tilemap_image = rl.loadImage("assets/images/dungeon_tilemap.png");
+    const tilemap_texture = rl.loadTextureFromImage(tilemap_image);
+    // const scarfy: rl.Texture = rl.Texture.init("assets/images/scarfy.png");
+
+    const editor_camera: rl.Camera2D = .{
+        .offset = rl.Vector2.init(0.0, 0.0),
+        .target = rl.Vector2.init(0.0, 0.0),
+        .rotation = 0.0,
+        .zoom = 4.0,
+    };
+
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
 
+        editor_camera.begin();
+        defer editor_camera.end();
+
         rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+
+        tilemap_texture.draw(0, 0, rl.Color.white);
     }
 }
