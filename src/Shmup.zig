bfe90864f60d8cb563a7ab9365eb91f5e1e78fc8
Scott Lambert <s-lambert@live.com.au>
2024-12-21 12:30:12 +0800

    Add enemy bullets

--- src/Shmup.zig
+++ src/Shmup.zig
@@ -22,6 +22,7 @@ const Bullet = struct {
 const SHOW_HITBOXES = true;
 const MAX_BULLETS = std.math.pow(usize, 4, 2);
 const MAX_ENEMIES = std.math.pow(usize, 4, 2);
+const MAX_ENEMY_BULLETS = std.math.pow(usize, 8, 2);
 
 const ShmupState = struct {
     player: struct {
@@ -33,6 +34,8 @@ const ShmupState = struct {
     spawn_cooldown: f32,
     next_enemy: usize,
     enemies: [MAX_ENEMIES]Enemy,
+    next_bullet: usize,
+    enemy_bullets: [MAX_ENEMY_BULLETS]Bullet,
 };
 
 var shmup_state: ShmupState = undefined;
@@ -58,6 +61,8 @@ pub fn preload() void {
         .spawn_cooldown = 0.0,
         .next_enemy = 0,
         .enemies = std.mem.zeroes([MAX_ENEMIES]Enemy),
+        .next_bullet = 0,
+        .enemy_bullets = std.mem.zeroes([MAX_ENEMY_BULLETS]Bullet),
     };
 }
 
@@ -110,6 +115,24 @@ pub fn update() !void {
         }
     }
 
+    for (&shmup_state.enemy_bullets) |*enemy_bullet| {
+        if (enemy_bullet.is_alive) {
+            enemy_bullet.*.time_alive += delta;
+            enemy_bullet.area.y += delta * 100.0;
+            if (enemy_bullet.kind == BulletKind.Wave) {
+                enemy_bullet.area.x = enemy_bullet.initial_x + 20.0 * @sin(enemy_bullet.time_alive * 5.0);
+            }
+            if (enemy_bullet.area.y <= 0.0) {
+                enemy_bullet.is_alive = false;
+            }
+
+            if (circle_collision(&player.position, &enemy_bullet.area)) {
+                enemy_bullet.is_alive = false;
+                std.debug.print("Player hit.", .{});
+            }
+        }
+    }
+
     if (shmup_state.spawn_cooldown > 0.0) {
         shmup_state.spawn_cooldown -= delta;
     } else {
@@ -131,7 +154,10 @@ pub fn update() !void {
 
     for (&shmup_state.enemies) |*enemy| {
         if (enemy.is_alive) {
-            enemy.update_func(enemy, delta);
+            const should_shoot = enemy.update_func(enemy, delta);
+            if (should_shoot) {
+                fire_enemy_bullet(enemy);
+            }
 
             for (&player.bullets) |*bullet| {
                 if (bullet.is_alive) {
@@ -143,6 +169,22 @@ pub fn update() !void {
             }
         }
     }
+}
+
+fn fire_enemy_bullet(enemy: *Enemy) void {
+    const bullet = &shmup_state.enemy_bullets[shmup_state.next_bullet];
+    std.debug.assert(!bullet.is_alive);
+
+    bullet.*.is_alive = true;
+    bullet.*.time_alive = 0.0;
+    bullet.*.area.x = enemy.*.area.x;
+    bullet.*.initial_x = bullet.*.area.x;
+    bullet.*.area.y = enemy.*.area.y - enemy.*.area.radius;
+    bullet.*.area.radius = 4.0;
+    bullet.*.kind = BulletKind.Straight;
+
+    shmup_state.next_bullet += 1;
+    shmup_state.next_bullet &= MAX_ENEMY_BULLETS - 1;
 }
 
 pub fn draw() void {
@@ -207,6 +249,26 @@ pub fn draw() void {
                 .{ .x = enemy.area.x, .y = enemy.area.y, .width = 32.0, .height = 32.0 },
                 .{ .x = 16.0, .y = 16.0 },
                 180.0,
+                rl.Color.white,
+            );
+        }
+    }
+
+    for (shmup_state.enemy_bullets) |enemy_bullet| {
+        if (enemy_bullet.is_alive) {
+            if (SHOW_HITBOXES) {
+                rl.drawCircle(
+                    @intFromFloat(enemy_bullet.area.x),
+                    @intFromFloat(enemy_bullet.area.y),
+                    enemy_bullet.area.radius,
+                    rl.Color.green,
+                );
+            }
+            bullet_spritesheet.drawPro(
+                .{ .x = 0.0, .y = 0.0, .width = 16.0, .height = 16.0 },
+                .{ .x = enemy_bullet.area.x, .y = enemy_bullet.area.y, .width = 16.0, .height = 16.0 },
+                .{ .x = 8.0, .y = 8.0 },
+                0.0,
                 rl.Color.white,
             );
         }
--- src/shmup/Enemy.zig
+++ src/shmup/Enemy.zig
@@ -1,7 +1,7 @@
 const Circle = @import("Hitbox.zig").Circle;
 
 pub const EnemyKind = enum { Shooting };
-const EnemyUpdateFn = *allowzero const fn (*Enemy, f32) void;
+const EnemyUpdateFn = *allowzero const fn (*Enemy, f32) bool;
 
 is_alive: bool,
 time_alive: f32,
@@ -9,15 +9,25 @@ area: Circle,
 kind: EnemyKind,
 update_func: EnemyUpdateFn,
 state: usize,
+fire_cooldown: f32,
 
 const Enemy = @This();
 
-fn entering_enemy_update(enemy: *Enemy, delta: f32) void {
+fn entering_enemy_update(enemy: *Enemy, delta: f32) bool {
     if (enemy.is_alive) {
         if (enemy.area.y <= 120.0) {
             enemy.area.y += delta * 20.0;
+        } else {
+            if (enemy.fire_cooldown > 0.0) {
+                enemy.fire_cooldown -= delta;
+            } else {
+                enemy.fire_cooldown = 2.0;
+                return true;
+            }
         }
     }
+
+    return false;
 }
 
 pub const EnemyUpdateFunctions = [_]EnemyUpdateFn{
